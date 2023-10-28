0f5d1c29135b2414038fd71d523a027bd0c68146
simrunsharma <141798228+simrunsharma@users.noreply.github.com>
2023-10-28 22:55:07 +0000

    very nice

--- rust_crud/src/main.rs
+++ rust_crud/src/main.rs
@@ -1,103 +1,113 @@
-extern crate rusqlite;
 extern crate csv;
+extern crate env_logger;
+extern crate log;
+extern crate rusqlite;
 
-use rusqlite::{params,Connection, Result};
-use csv::ReaderBuilder;
+use csv::Reader;
+use rusqlite::{Connection, Result};
+use std::env;
+use std::error::Error;
+use std::fs::File;
+use std::io;
+use std::io::Write;
+use std::process;
 
-fn create(database_name: &str) -> Result<Vec<Vec<String>>> {
-    let conn = Connection::open(database_name)?;
-    let mut reader = ReaderBuilder::new()
-        .has_headers(true)
-        .from_path("/workspaces/Individual_Project_2_Simrun/data/auto_data.csv")?;
-    let mut stmt = conn.prepare("CREATE TABLE IF NOT EXISTS auto (id INTEGER PRIMARY KEY, make TEXT, model TEXT, year INTEGER, horsepower INTEGER, price INTEGER)")?;
-    stmt.execute(params![])?;
-    let mut insert_stmt = conn.prepare("INSERT INTO auto (make, model, year, horsepower, price) VALUES (?, ?, ?, ?, ?)")?;
-    for result in reader.records() {
+fn main() -> Result<(), Box<dyn Error>> {
+    // Get the SQLite database file path and CSV file path from command-line arguments
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 3 {
+        eprintln!("Usage: {} <database_file> <csv_file>", args[0]);
+        process::exit(1);
+    }
+
+    let db_path = &args[1];
+    let csv_file_path = &args[2];
+
+    // Open the SQLite database connection
+    let conn = Connection::open(db_path)?;
+
+    // Create a table to store the CSV data
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS data (column1 TEXT, column2 TEXT, column3 TEXT)",
+        [],
+    )?;
+
+    // Open the CSV file and insert its data into the SQLite database
+    let file = File::open(csv_file_path)?;
+    let mut rdr = Reader::from_reader(file);
+
+    for result in rdr.records() {
         let record = result?;
-        insert_stmt.execute(params![record[0], record[1], record[2], record[3], record[4]])?;
-    }
-    let mut select_stmt = conn.prepare("SELECT * FROM auto")?;
-    let results = select_stmt.query_map(params![], |row| {
-        Ok(vec![
-            row.get(0)?,
-            row.get(1)?,
-            row.get(2)?,
-            row.get(3)?,
-            row.get(4)?,
-            row.get(5)?,
-        ])
-    })?;
-    let mut result_vec = Vec::new();
-    for result in results {
-        result_vec.push(result?);
-    }
-    Ok(result_vec)
-}
-
-
-fn read(database_name: &str) -> Result<(Vec<Vec<String>>, Vec<Vec<String>>, Vec<Vec<String>>)> {
-    let conn = Connection::open(database_name)?;
-    let mut result1 = Vec::new();
-    let mut cursor = conn.prepare("SELECT \"model year\" FROM auto WHERE \"car name\" == \"amc hornet\"")?.query(params![])?;
-    while let Some(row) = cursor.next()? {
-        result1.push(vec![row.get(0)?]);
-    }
-    println!("");
-
-    let mut result2 = Vec::new();
-    cursor = conn.prepare("SELECT \"car name\", \"cylinders\" FROM auto WHERE \"cylinders\" > 6")?.query(params![])?;
-    while let Some(row) = cursor.next()? {
-        result2.push(vec![row.get(0)?, row.get(1)?]);
+        conn.execute(
+            "INSERT INTO data (column1, column2, column3) VALUES (?1, ?2, ?3)",
+            [&record[0], &record[1], &record[2]],
+        )?;
     }
 
-    println!("");
+    println!("CSV data imported into the SQLite database.");
 
-    let mut result3 = Vec::new();
-    cursor = conn.prepare("SELECT \"car name\", \"cylinders\" FROM auto WHERE (\"cylinders\" < 8) AND (\"car name\" == \"amc hornet\")")?.query(params![])?;
-    while let Some(row) = cursor.next()? {
-        result3.push(vec![row.get(0)?, row.get(1)?]);
+    // Rename the columns using SQL
+    conn.execute("ALTER TABLE data RENAME TO data_old", [])?;
+    conn.execute(
+        "CREATE TABLE data AS SELECT column1 AS mpg, column2 AS cylinders, column3 AS displacement FROM data_old",
+        [],
+    )?;
+    conn.execute("DROP TABLE data_old", [])?;
+
+    // Allow SQL queries from the terminal
+    loop {
+        let mut input = String::new();
+        print!("Enter an SQL query (or 'exit' to quit): ");
+        io::stdout().flush().expect("Failed to flush stdout");
+
+        io::stdin().read_line(&mut input)?;
+
+        let query = input.trim();
+        if query == "exit" {
+            break;
+        }
+
+        match conn.execute_batch(query) {
+            Ok(_) => {
+                println!("Query executed successfully.");
+
+                if query.to_lowercase().starts_with("select") {
+                    let mut stmt = conn.prepare(query)?;
+
+                    // Get the column names
+                    let columns: Vec<String> = stmt
+                        .column_names()
+                        .iter()
+                        .map(|col_name| col_name.to_string())
+                        .collect();
+
+                    let header = columns.join(" | ");
+                    println!("{}", header);
+
+                    let rows = stmt.query_map([], |row| {
+                        let values: Vec<String> = columns
+                            .iter()
+                            .enumerate()
+                            .map(|(i, _)| {
+                                match row.get(i) {
+                                    Ok(value) => value,
+                                    Err(_) => "NULL".to_string(), // Handle NULL values if necessary.
+                                }
+                            })
+                            .collect();
+                        Ok(values)
+                    })?;
+                    for row in rows {
+                        if let Ok(row_data) = row {
+                            let row_str = row_data.join(" | ");
+                            println!("{}", row_str);
+                        }
+                    }
+                }
+            }
+            Err(err) => eprintln!("Error executing query: {}", err),
+        }
     }
 
-    conn.commit()?;
-    Ok((result1, result2, result3))
-}
-
-fn update(database_name: &str) -> Result<Vec<Vec<String>>> {
-    let conn = Connection::open(database_name)?;
-    conn.execute("UPDATE auto SET \"cylinders\" = '4' WHERE \"cylinders\" = '3'", params![])?;
-    let mut cursor = conn.prepare("SELECT * FROM auto")?.query(params![])?;
-    let mut results = Vec::new();
-    while let Some(row) = cursor.next()? {
-        results.push(vec![
-            row.get(0)?,
-            row.get(1)?,
-            row.get(2)?,
-            row.get(3)?,
-            row.get(4)?,
-            row.get(5)?,
-        ]);
-    }
-    conn.commit()?;
-    Ok(results)
-}
-
-fn delete(database_name: &str) -> Result<Vec<Vec<String>>> {
-    let conn = Connection::open(database_name)?;
-    conn.execute("DELETE FROM auto WHERE \"cylinders\" = 5", params![])?;
-    let mut cursor = conn.prepare("SELECT \"car name\" FROM auto")?.query(params![])?;
-    let mut results = Vec::new();
-    while let Some(row) = cursor.next()? {
-        results.push(vec![row.get(0)?]);
-    }
-    conn.commit()?;
-    Ok(results)
-}
-
-fn main() -> Result<()> {
-    let database_name = "auto.db";
-    create(database_name)?;
-    read(database_name)?;
-    update(database_name)?;
-    delete(database_name)?;
     Ok(())
 }
